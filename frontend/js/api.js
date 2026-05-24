2e408ffe397465f74730784adb53290b05e0fd15
kazenaka <kazenaka@gmail.com>
2026-05-24 16:22:07 +0300

    заменил локальный адрес на твой облачный от Render

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api/v1'; 
+const API_BASE_URL = 'https://cybershop-api-sdyt.onrender.com/api/v1';
 
 const API = {
     // Получение токена из LocalStorage
